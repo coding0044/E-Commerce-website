79e5f0d2f1121ef3bd8e75bf01265e71bfbf8758
Faraz <SP24BSCS0044@maju.edu.pk>
2025-12-23 23:52:21 +0500

    Intership Project

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./pages/About";
-import Home from "./pages/Home"; 
-import Contact from "./pages/Contact"; 
-import Login from "./pages/Login"; 
-import Register from "./pages/Register"; 
-import Categories from "./pages/Categories"; 
-import Products from "./pages/Products"; 
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Categories from "./pages/Categories";
+import Products from "./pages/Products";
 
 
 
@@ -28,11 +28,11 @@ const App = () => {
         <Route path="/categories" element={<Categories />} />
         <Route path="/products" element={<Products />} />
 
-        
+
       </Routes>
       <Footer />
     </Router>
-    
+
   );
 };
 
